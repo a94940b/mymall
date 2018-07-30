73cbdf10f7bca607cedfbf37ea029edcf2ff47fd
bohan <784368611@qq.com>
2018-07-30 18:56:30 +0800

    Initial commit

---
+++ src/Title.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+   这是商品标题
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+   这是商品列表页面
+   <span>{{$route.params.goodsId}}</span><br>
+   <span>{{$route.params.username}}</span>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
