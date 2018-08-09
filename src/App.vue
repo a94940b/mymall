b89c302b62d1cc346ea4866f397998ce3cb3222b
bohan <784368611@qq.com>
2018-08-09 16:41:42 +0800

    商品列表页面组件拆分

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view class="main"></router-view>
-    命名视图，可以实现根页面加载出来三个不同的组件，实现布局效果
-    <router-view name="title" class="left"></router-view>
-    <router-view name="image" class="right"></router-view>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.main {
-  font-size: 30px;
-}
-.left,
-.right {
-  float: left;
-  width: 49%;
-  border: 1px solid #eee;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodsList from './../views/GoodsList'
-import Title from '@/views/Title'
-import Image from '@/views/Image'
-import Cart from '@/views/Cart'
 
-Vue.use(Router)
+import GoodsList from '@/views/goodslist/GoodsList'
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    { 
+    {
       path: '/',
       name: 'GoodsList',
-      components: {
-        default: GoodsList,
-        title: Title,
-        image: Image
-      }
-    },
-    {
-      path: '/cart/:cartId',
-      name: 'cart',
-      component: Cart
+      component: GoodsList
     }
   ]
 })
